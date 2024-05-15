fbf899847fab044e1464cd5b9db6c315d182fb4a
Santhanam-san <ranaranran@gmail.com>
2024-05-15 18:31:50 +0530

    Add project files.

---
+++ JokesApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string jokequestion { get; set; }
+        public string jokeanswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+
+    }
+}
